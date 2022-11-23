a78df966fbb3f99ebce459282fafa0255bfdce7f
agwadan <agwa.dan@gmail.com>
2022-11-23 15:26:01 +0300

    Add winning patterns

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import { Square } from './components/Square';
 import './App.css';
 
@@ -10,10 +9,8 @@ function App() {
   const chooseSquare = (square) => {
     setBoard(board.map((val, idx) => {
       if (idx == square && val == "") {
-        console.log(player);
         return player;
       }
-      console.log(val);
       return val;
     }));
 
@@ -30,8 +27,16 @@ function App() {
           <Square val={board[1]} chooseSquare={() => chooseSquare(1)} />
           <Square val={board[2]} chooseSquare={() => chooseSquare(2)} />
         </div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Square val={board[3]} chooseSquare={()=> chooseSquare(3)} />
+          <Square val={board[4]} chooseSquare={() => chooseSquare(4)} />
+          <Square val={board[5]} chooseSquare={() => chooseSquare(5)} />
+        </div>
+        <div className="row">
+          <Square val={board[6]} chooseSquare={()=> chooseSquare(6)} />
+          <Square val={board[7]} chooseSquare={() => chooseSquare(7)} />
+          <Square val={board[8]} chooseSquare={() => chooseSquare(8)} />
+        </div>
       </div>
     </div>
   )
